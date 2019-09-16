916aa2a6b191528ce71899680f7c27a275cf7f0d
ho-un Woo <woohoeon@gmail.com>
2019-09-16 23:29:09 +0900

    Updates

---
+++ precache-manifest.a67187b497800d124754c198880b8174.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef03e9873f51bdeeb8f450d167ce5a7",
+    "url": "/feedback/index.html"
+  },
+  {
+    "revision": "b36b4309b5c722334e13",
+    "url": "/feedback/static/js/2.4d6a1316.chunk.js"
+  },
+  {
+    "revision": "a208e80225b09de9bab3",
+    "url": "/feedback/static/js/main.9f9bc089.chunk.js"
+  },
+  {
+    "revision": "825556bc22dbae034978",
+    "url": "/feedback/static/js/runtime~main.b6c88840.js"
+  }
+]);
